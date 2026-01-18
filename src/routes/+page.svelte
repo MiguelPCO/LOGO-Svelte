2628debc477cbf9a80941fe1495c21bcd326b7d0
Miguel Castillo <mc@iberbox.com>
2026-01-18 17:53:55 +0100

    feat: Add product list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    type Product ={
+        id: number;
+        name: string;
+        price: number;
+    }
+
+    const PRODUCTS: Product[] = [
+        {id: 1, name: 'manzana', price: 1},
+        {id: 2, name: 'pera', price: 1.5},
+        {id: 3, name: 'uva', price: 2.5},
+        {id: 4, name: 'sandía', price: 6},
+        {id: 5, name: 'kiwi', price: 3.8},
+    ]
+</script>
+<main class="container mx-auto p-4">
+	<h1 class="text-2xl font-bold mb-6">Frutería LOGO</h1>
+
+	<section class="mb-8">
+		<h2 class="text-xl font-semibold mb-4">Productos Disponibles</h2>
+		<div class="flex gap-3 flex-wrap">
+			{#each PRODUCTS as product}
+				<button 
+					class="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+				>
+					{product.name} - {product.price.toFixed(2)}€
+				</button>
+			{/each}
+		</div>
+	</section>
+
+</main>
